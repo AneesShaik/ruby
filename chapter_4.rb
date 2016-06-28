4fd89fb75881fff80db0f46186c6ab0b989244df
anees.shaik <anees.shaik@reancloud.com>
2016-06-28 04:00:51 +0000

    This chapter deals with the Ruby String operation handling

---
+++ chapter_4.rb
@@ -0,0 +1,18 @@
+puts "The following program will greet you after you input your name"
+
+puts "Please enter the follwing details"
+
+print "First Name: "
+first_name = gets.chomp
+print "Middle Name: "
+middle_name = gets.chomp
+print "Last Name: "
+last_name = gets.chomp
+puts "Hello "+first_name+" "+middle_name+" "+last_name
+puts ""
+puts "This is the 2nd Question in the exercise which outputs the Bigger and better number after taking your favourite number as input"
+print "Enter your favourite number: "
+fav_num = gets.chomp
+puts "The bigger and better number is #{ fav_num.to_i + 1 }"
+
+
