0ab8e64d9e66a685b154f079986b444cb7fadcde
anees.shaik <anees.shaik@reancloud.com>
2016-06-30 20:45:05 +0000

    completed the exercise following the guidelines and Ruby-style guide

---
+++ chapter_5/question_1.rb
@@ -0,0 +1,11 @@
+puts "ANGRY BOSS PROGRAM".center(50)
+
+puts ""
+puts "Enter 'I want a raise' when employer input is prompted"
+puts "Boss: What do you want? "
+
+print "Employer: "
+reply = gets.chomp
+
+puts "Boss: WHADDAYA MEAN \"I WANT A RAISE.\"?!?  YOU'RE FIRED!!" #Escape character is being implemented
+
---
+++ chapter_5/question_2.rb
@@ -0,0 +1,8 @@
+linewidth = 50
+
+puts "Display Table of contents".center(linewidth)
+puts "Table of Contents".center(linewidth)
+puts "Chapter 1: Numbers".ljust(linewidth/2) + "page 1".rjust(linewidth/2)
+puts "Chapter 2: Letters".ljust(linewidth/2) + "page 72".rjust(linewidth/2)
+puts "Chapter 3: Variables".ljust(linewidth/2) + "page 118".rjust(linewidth/2)
+
