4bf1602ab0137157ca34206386363ad15102f167
anees.shaik <anees.shaik@reancloud.com>
2016-06-30 20:41:53 +0000

    Divided the code acordingly for better understanding and also included the interpolation method to output variables

--- chapter_4/question_1.rb
+++ chapter_4/question_1.rb
@@ -1,6 +1,6 @@
-# 1 Question
 puts "The following program will greet you after you input your name"
 
+puts ""
 puts "Please enter the follwing details"
 
 print "First Name: "
@@ -9,6 +9,8 @@ print "Middle Name: "
 middle_name = gets.chomp
 print "Last Name: "
 last_name = gets.chomp
-puts "Hello " + first_name.capitalize + " " + middle_name.capitalize + " " + last_name.capitalize
+
+puts ""
+puts "Hello #{first_name.capitalize} #{middle_name.capitalize} #{last_name.capitalize}" # Uses interpolation method to output the variable values
 
 
--- chapter_4/question_2.rb
+++ chapter_4/question_2.rb
@@ -1,4 +1,8 @@
-puts "In this exercise which outputs the Bigger and better number after taking your favourite number as input"
+puts "This exercise outputs the Bigger and Better number after taking your favourite number as input"
+
+puts ""
 print "Enter your favourite number: "
 fav_num = gets.chomp
+
+puts ""
 puts "The bigger and better number is #{ fav_num.to_i + 1 }"
