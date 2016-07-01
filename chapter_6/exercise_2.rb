c897b9c0924fe093d88dd5560dd423dcc89d2781
anees.shaik <anees.shaik@reancloud.com>
2016-07-01 18:18:27 +0000

    Updated exercise_2.rb without spacing and replaced empty puts statement with eascape sequence

--- chapter_6/exercise_2.rb
+++ chapter_6/exercise_2.rb
@@ -1,12 +1,10 @@
-puts "Deaf Grandma Program".center(50)
-puts ""
+puts "Deaf Grandma Program\n\n".center(50)
 
-puts "You're Sonny, start the program by speaking to the Grandma. She will respond only if you type your reply in CAPITAL LETTERS"
-puts ""
+puts "You're Sonny, start the program by speaking to the Grandma. She will respond only if you type your reply in CAPITAL LETTERS\n\n"
+
 print "Sonny: "
 
 while (reply = gets.chomp) != "bye".upcase
-
   if reply == reply.upcase
     puts "Grandma: NO, NOT SINCE #{1930 + rand(20)}!"
   else
@@ -14,7 +12,6 @@ while (reply = gets.chomp) != "bye".upcase
   end
 
   print "Sonny: "
-
 end
 
 
