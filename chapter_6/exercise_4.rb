1d1f89a511db2187e122672e1b011bd32d3a3b2c
anees.shaik <anees.shaik@reancloud.com>
2016-07-01 18:41:07 +0000

    Operator spacing added

--- chapter_6/exercise_4.rb
+++ chapter_6/exercise_4.rb
@@ -10,7 +10,7 @@ end_year = gets.chomp.to_i
 
 puts "Leap years between #{start_year} and #{end_year}:"
 
-while start_year != end_year+1
+while start_year != end_year + 1
   puts start_year if Date.new(start_year).leap?
   start_year += 1
 end
