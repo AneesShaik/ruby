3dceeb300aea3e9e5014064ab6dfb4be1a3da166
anees.shaik <anees.shaik@reancloud.com>
2016-07-01 15:45:47 +0000

    Included snake case on multi-word variables and spacing between operators

--- chapter_5/question_2.rb
+++ chapter_5/question_2.rb
@@ -1,8 +1,8 @@
-linewidth = 50
+line_width = 50
 
-puts "Display Table of contents".center(linewidth)
-puts "Table of Contents".center(linewidth)
-puts "Chapter 1: Numbers".ljust(linewidth/2) + "page 1".rjust(linewidth/2)
-puts "Chapter 2: Letters".ljust(linewidth/2) + "page 72".rjust(linewidth/2)
-puts "Chapter 3: Variables".ljust(linewidth/2) + "page 118".rjust(linewidth/2)
+puts "Display Table of contents".center(line_width)
+puts "Table of Contents".center(line_width)
+puts "Chapter 1: Numbers".ljust(line_width / 2) + "page 1".rjust(line_width / 2)
+puts "Chapter 2: Letters".ljust(line_width / 2) + "page 72".rjust(line_width / 2)
+puts "Chapter 3: Variables".ljust(line_width / 2) + "page 118".rjust(line_width / 2)
 
