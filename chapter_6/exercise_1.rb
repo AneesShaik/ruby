f496b7c60134896e50b236e7ab1b4b47d6cd32bb
anees.shaik <anees.shaik@reancloud.com>
2016-07-01 18:16:22 +0000

    Updated exercise_1.rb without spacing after expressions

--- chapter_6/exercise_1.rb
+++ chapter_6/exercise_1.rb
@@ -3,13 +3,10 @@ puts "99 beer bottles lyrics".center(50)
 num = 99
 
 while num != 0
-
   puts "#{num} bottles of beer on  the wall, #{num} bottles of beer."
-  puts "Take one dwon and pass it around, #{num-1} bottles of beer on the wall."
-  puts ""
+  puts "Take one dwon and pass it around, #{num - 1} bottles of beer on the wall.\n\n"
 
   num -= 1
-
 end
 
 puts "No more bottles of beer on the wall, no more bottles of beer."
