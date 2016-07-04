6618a98a189fca5a12fb507aed1f23ee5b561807
anees.shaik <anees.shaik@reancloud.com>
2016-07-04 03:45:30 +0000

    Exercise 3 loop issue resolved

--- chapter_7/exercise_3.rb
+++ chapter_7/exercise_3.rb
@@ -1,14 +1,12 @@
 toc = ["Table of Contents", "Chapter 1", "Getting Started", "page 1", "Chapter 2", "Numbers", "page 9", "Chapter 3", "Letter", "page 13"]
 
 line_width = 50
-array_size = toc.length # Calculates the length of array
 i = 1
 
 puts "#{toc[0].center(line_width * 2)}\n\n"
 
-while array_size != 0
+while i < toc.length
   puts "#{toc[i]}: #{toc[i + 1].ljust(line_width)} #{toc[i + 2].rjust(line_width)}" # Iterating line to print all the array elements & Spacing not included after ":"
   i += 3
-  array_size -= 3
 end
 
