371464e13dd85d07b93f68778babbd73d5732360
anees.shaik <anees.shaik@reancloud.com>
2016-07-05 17:11:28 +0000

    Updated the spacing and indentation issues

--- chapter_8/exercise_1.rb
+++ chapter_8/exercise_1.rb
@@ -1,12 +1,13 @@
-def englishNumber number
+def english_number(number)
   if number < 0 
-	return "Please enter a number that isn't negative."
-  end
-  if number == 0
-	return 'zero'
+    return "Please enter a number that isn't negative."
   end
 
-  numString = "" 
+  if number == 0
+    return "zero"
+  end
+
+  num_string = "" 
 
   ones = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
   tens = ['Ten', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
@@ -14,76 +15,81 @@ def englishNumber number
 
   left = number
 
-  write = left / 1000000000 
-  left -= write * 1000000000 
+  right = left / 1000000000 
+  left -= right * 1000000000 
 
-  if write > 0
-    billions = englishNumber write
-    numString += billions + " billion"
+  if right > 0
+    billions = english_number(right)
+    num_string += billions + " billion"
+
     if left > 0
-      numString += " and "
+      num_string += " and "
     end
   end
 
-  write = left / 1000000 
-  left -= write * 1000000 
+  right = left / 1000000 
+  left -= right * 1000000 
 
-  if write > 0
-    millions = englishNumber write
-    numString += millions + " million"
+  if right > 0
+    millions = english_number(right)
+    num_string += millions + " million"
+
     if left > 0
-      numString += " and "
+      num_string += " and "
     end
   end
 
-  write = left / 1000 
-  left -= write * 1000 
+  right = left / 1000 
+  left -= right * 1000 
 
-  if write > 0
-    thousands = englishNumber write
-    numString += thousands + " thousand"
+  if right > 0
+    thousands = english_number(right)
+    num_string += thousands + " thousand"
+
     if left > 0
-      numString += " and "
+      num_string += " and "
     end
   end
 
-  write = left / 100 
-  left -= write * 100
+  right = left / 100 
+  left -= right * 100
 
-  if write > 0
-    teens = englishNumber write
-    numString += teens + " hundred"
+  if right > 0
+    teens = english_number(right)
+    num_string += teens + " hundred"
+
     if left > 0
-      numString += " and "
+      num_string += " and "
     end
   end
 
-  write = left / 10 
-  left -= write * 10 
+  right = left / 10 
+  left -= right * 10 
 
-  if write > 0
-    if ((write == 1) and (left > 0))
-      numString += teens[left - 1]
+  if right > 0
+    if ((right == 1) and (left > 0))
+      num_string += teens[left - 1]
       left = 0
     else
-      numString += tens[write - 1]
+      num_string += tens[right - 1]
     end
 
     if left > 0
-      numString += "-"
+      num_string += "-"
     end
   end
 
-  write = left 
+  right = left 
   left = 0 
 
-  if write > 0
-    numString += ones[write - 1]
+  if right > 0
+    num_string += ones[right - 1]
   end
 
-  numString
+  num_string
 end
 
 print "Please Enter a number: "
 output = gets.chomp.to_i
-puts englishNumber(output)
+puts english_number(output)
+
--- chapter_8/exercise_2.rb
+++ chapter_8/exercise_2.rb
@@ -1,12 +1,13 @@
-def englishNumber number
+def english_number(number)
   if number < 0 
     return "Please enter a number that isn't negative."
   end
+
   if number == 0
-   return 'zero'
+    return "zero"
   end
 
-  numString = "" 
+  num_string = "" 
 
   ones = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
   tens = ['Ten', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
@@ -14,75 +15,78 @@ def englishNumber number
 
   left = number
 
-  write = left / 1000000000 
-  left -= write * 1000000000 
+  right = left / 1000000000 
+  left -= right * 1000000000 
 
-  if write > 0
-    billions = englishNumber write
-    numString += billions + " billion"
+  if right > 0
+    billions = english_number(right)
+    num_string += billions + " billion"
+
     if left > 0
-      numString += " and "
+      num_string += " and "
     end
   end
 
-  write = left / 1000000 
-  left -= write * 1000000 
+  right = left / 1000000 
+  left -= right * 1000000 
 
-  if write > 0
-    millions = englishNumber write
-    numString += millions + " million"
+  if right > 0
+    millions = english_number(right)
+    num_string += millions + " million"
+
     if left > 0
-      numString += " and "
+      num_string += " and "
     end
   end
 
-  write = left / 1000 
-  left -= write * 1000 
+  right = left / 1000 
+  left -= right * 1000 
 
-  if write > 0
-    thousands = englishNumber write
-    numString += thousands + " thousand"
+  if right > 0
+    thousands = english_number(right)
+    num_string += thousands + " thousand"
+
     if left > 0
-      numString += " and "
+      num_string += " and "
     end
   end
 
-  write = left / 100 
-  left -= write * 100
+  right = left / 100 
+  left -= right * 100
 
-  if write > 0
-    teens = englishNumber write
-    numString += teens + " hundred"
+  if right > 0
+    teens = english_number(right)
+    num_string += teens + " hundred"
+
     if left > 0
-      numString += " and "
+      num_string += " and "
     end
   end
 
-  write = left / 10 
-  left -= write * 10 
+  right = left / 10 
+  left -= right * 10 
 
-  if write > 0
-    if ((write == 1) and (left > 0))
-      numString += teens[left - 1]
+  if right > 0
+    if ((right == 1) and (left > 0))
+      num_string += teens[left - 1]
       left = 0
     else
-    
-    numString += tens[write - 1]
+      num_string += tens[right - 1]
     end
 
     if left > 0
-      numString += "-"
+      num_string += "-"
     end
   end
 
-  write = left 
+  right = left 
   left = 0 
 
-  if write > 0
-	numString += ones[write - 1]
+  if right > 0
+    num_string += ones[right - 1]
   end
 
-  numString
+  num_string
 end
 
 puts "99 beer bottles lyrics".center(50)
@@ -90,12 +94,11 @@ puts "99 beer bottles lyrics".center(50)
 output = 99
 
 while output != 0
-  puts "#{englishNumber(output)} bottles of beer on  the wall, #{englishNumber(output)} bottles of beer."
-  puts "Take one dwon and pass it around, #{englishNumber(output - 1)} bottles of beer on the wall.\n\n"
+  puts "#{english_number(output)} bottles of beer on  the wall, #{english_number(output)} bottles of beer."
+  puts "Take one dwon and pass it around, #{english_number(output - 1)} bottles of beer on the wall.\n\n"
 
   output -= 1
 end
 
 puts "No more bottles of beer on the wall, no more bottles of beer."
 puts "Got to the store and buy some more, Ninety-Nine bottles of beer on the wall."
-
